7600b9e7090cd33d96d02fe07b94bd8066ed4161
rafasu4 <raf59101@gmail.com>
2021-03-16 22:06:06 +0200

    small menu added

--- main.cpp
+++ main.cpp
@@ -7,42 +7,17 @@
 using namespace std;
 using namespace ariel;
 
-int main(){
-    cout << snowman(22222222) << endl;/* Should print
-    ___
-   .....
-  \(o.o)/
-   (] [)
-   (" ")
-     */
-    cout << snowman(33333333) << endl;   /* Should print:
-   _
-  /_\
- (O_O)
-/(> <)\
- (___)
-	*/
-    cout << snowman(44444444) << endl;   /* Should print:
- ___
-(_*_)
-(- -)
-(   )
-(   )
-	*/
-
-    cout << snowman(43432214) << endl;   /* Should print:
-  ___
- (_*_)
-\(-_O)/
- ( : )
- (   )
-	*/
-    cout << snowman(13213244) << endl;   /* Should print:
- _===_
- (o_.)/
-/(   )
- (   )
-
-	*/
+int main() {
+    int letter = 0;
+    int number = 0;
+    do {
+        cout << endl;
+        cout << "Welcome to Snowman program!" << endl;
+        cout << "Enter an 8 digits number between 1 - 4 for a snowman figure: ";
+        cin >> number;
+        cout << snowman(number);
+        cout << endl << "For another run enter any number. To exit enter 0: ";
+        cin >> letter;
+    }while (letter != 0);
     return 0;
 }
